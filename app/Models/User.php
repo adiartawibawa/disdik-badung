8472e692be57b15a35e2786eafdc84db7919a71f
Adi Arta Wibawa <75978314+adiartawibawa@users.noreply.github.com>
2023-10-10 10:47:39 +0800

    add media library

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,16 +4,19 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements HasMedia
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles, HasUuids;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles, HasUuids, InteractsWithMedia;
 
     /**
      * The attributes that are mass assignable.
@@ -45,4 +48,23 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected $appends = [
+        'avatar_url'
+    ];
+
+    public function getCreatedAtFormattedAttribute()
+    {
+        return Carbon::parse($this->attributes['created_at'])->format('d, M Y H:i:s');
+    }
+
+    public function getUpdatedAtFormattedAttribute()
+    {
+        return Carbon::parse($this->attributes['updated_at'])->format('d, M Y H:i:s');
+    }
+
+    public function getVerifiedAtFormattedAttribute()
+    {
+        return Carbon::parse($this->attributes['email_verified_at'])->format('d, M Y H:i:s');
+    }
 }
